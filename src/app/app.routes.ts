ffc2c15fbb9aafa090ea26c61efd4362e4a1bbdf
MartinPPY <martinsantiago.se@gmail.com>
2025-08-26 20:28:21 -0400

    agregando autenticacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { ForgotPassword } from './auth/forgot-password/forgot-password';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'log-in', pathMatch: 'full' },
+    { path: 'log-in', component: Login },
+    { path: 'register', component: Register },
+    { path: 'forgot-password', component: ForgotPassword },
+    { path: '**', redirectTo: 'login' }
+];
