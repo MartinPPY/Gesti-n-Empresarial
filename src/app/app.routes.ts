9b1818d96c5238a4c0b43f5dd14cc9f2989520e6
MartinPPY <martinsantiago.se@gmail.com>
2025-08-28 17:16:12 -0400

    creacion de la vista de admin finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Admin } from './admin/admin';
 import { Dashboard } from './admin/components/dashboard/dashboard';
 import { Users } from './admin/components/users/users';
 import { Projects } from './admin/components/projects/projects';
+import { Task } from './admin/components/task/task';
+import { Resources } from './admin/components/resources/resources';
 
 
 export const routes: Routes = [
@@ -19,7 +21,9 @@ export const routes: Routes = [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
             { path: 'dashboard', component: Dashboard },
             { path: 'users', component: Users },
-            {path:'projects',component:Projects}
+            { path: 'projects', component: Projects },
+            { path: 'tasks', component: Task },
+            { path: 'resources',component: Resources}
         ]
     }
 ];
